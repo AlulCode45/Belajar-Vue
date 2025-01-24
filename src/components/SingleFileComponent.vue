bd10803c151342657a5b0de95cafb0651c05ccd4
AlulCode45 <alulgans70@gmail.com>
2025-01-24 09:01:23 +0700

    feat: single file component

---
+++ src/components/SingleFileComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+const name = ref("Alul");
+
+</script>
+
+
+<template>
+  <h1>Hello, {{ name }}🎉</h1>
+</template>
+
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
